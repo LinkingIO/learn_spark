3bdb4e6a81d5b8e780d55e204b08a03151592a9a
Logan <luologa@mfcgd.com>
2022-10-29 17:35:03 +0800

    collection: 练习word count 02

---
+++ src/main/scala/com/manulife/bigdata/scala/chapter07/collection01/Practice02_WordCount.scala
@@ -0,0 +1,16 @@
+package com.manulife.bigdata.scala.chapter07.collection01
+
+object Practice02_WordCount {
+  def main(args: Array[String]): Unit = {
+    val worldCountTop3: List[(String, Int)] = List("hello scala spark kafka", "hello scala spark", "hello scala", "hello")
+      .flatMap(elem => elem.split(" "))
+      .groupBy(word => word)
+      .map(tuple => (tuple._1, tuple._2.size))
+      .toList
+      .sortWith((left, right) => left._2 > right._2)
+      .take(3)
+    println(worldCountTop3.mkString(","))
+  }
+
+
+}
