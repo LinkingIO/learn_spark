88edce435d588b2b8346cbe2299796d5fa758b0b
luo <rockyluo@163.com>
2022-11-11 02:39:47 +0800

    add spark utils

---
+++ src/main/scala/com/manulife/bigdata/sparktuning/utils/bean/School.scala
@@ -0,0 +1,3 @@
+package com.manulife.bigdata.sparktuning.utils.bean
+
+case class School(id: Long, name: String, partition: Int)
---
+++ src/main/scala/com/manulife/bigdata/sparktuning/utils/bean/Student.scala
@@ -0,0 +1,3 @@
+package com.manulife.bigdata.sparktuning.utils.bean
+
+case class Student(id: Long, name: String, age: Int, partition: Int)
